728b37664cddd1a081c8c8116f052226be6fc764
GustavoLopez1307 <46933191+GustavoLopez1307@users.noreply.github.com>
2019-02-18 22:44:01 -0500

    Add files via upload

---
+++ talleres/taller04/Taller4.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.TimeUnit;
+public class Taller4
+{
+    public static int x = 0;
+    public static int SumaArray(int[] Array){
+       if(x== Array.length){
+         return 0; 
+       }
+       x += 1;
+       try{
+            TimeUnit.SECONDS.sleep(1); //Para obtener el tiempo de ejecución
+        }catch (Exception e){
+            System.out.println("Error: "+e);
+        }
+       return Array[x-1] + SumaArray(Array);
+    }
+    public  int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        try{
+            TimeUnit.SECONDS.sleep(1); //Para obtener el tiempo de ejecución
+        }catch (Exception e){
+            System.out.println("Error: "+e);
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
